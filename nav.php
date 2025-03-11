1f82efbead7181bf395760a187a1d44e508f3456
NoahCounsell <noahwcounsell@icloud.com>
2025-03-11 10:22:50 +0000

    change nav links to white | to contrast the banner

--- nav.php
+++ nav.php
@@ -6,10 +6,10 @@
               <img src="no_entry_studio_logo.png" alt="" class="h-full">
             </div>
             <div class="hidden md:flex space-x-8 relative z-20">
-              <a href="index.php" class="text-black text-xl font-extralight hover:text-gray-600 transition-colors">Home</a>
-              <a href="about.php" class="text-black text-xl font-extralight hover:text-gray-600 transition-colors">About</a>
-              <a href="#" class="text-black text-xl font-extralight hover:text-gray-600 transition-colors">Services</a>
-              <a href="#" class="text-black text-xl font-extralight hover:text-gray-600 transition-colors">Contact</a>
+              <a href="index.php" class="text-white text-xl font-extralight hover:text-gray-600 transition-colors">Home</a>
+              <a href="about.php" class="text-white text-xl font-extralight hover:text-gray-600 transition-colors">About</a>
+              <a href="#" class="text-white text-xl font-extralight hover:text-gray-600 transition-colors">Services</a>
+              <a href="#" class="text-white text-xl font-extralight hover:text-gray-600 transition-colors">Contact</a>
             </div>
             <div class="md:hidden relative z-20">
               <label for="menu-toggle" class="cursor-pointer text-black block p-2">
